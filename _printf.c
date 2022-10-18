400fd6c3ad30656873c8a7001252499d3b7707cd
Yordi-SE <yordanoslemmaworkofkind1@gmail.com>
2022-10-18 14:51:38 +0000

    project

--- _printf.c
+++ _printf.c
@@ -1,4 +1,4 @@
-#include "printf.h"
+#include "main.h"
 /**
  * _printf - prints anything
  * @format: list of types of arguments
@@ -41,69 +41,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(ap);
 	return (s);
-}
-/**
- * _print_num - print number
- * @y: number to be printed
- * @f: pointer to function
- * @b: pointer
- * @g: pointer
- * Return: void
- */
-void _print_num(int y, int (*f)(long int), int *b, int *g)
-{
-	*(b) = *(b) + f(y);
-	*(g) = (*g) + 1;
-}
-/**
- * _bool - print_all
- * Return: int
- * @format: string
- * @spec: string
- * @i: integer
- */
-int _bool(const char *format, char *spec, int i)
-{
-
-	if ((format[i] == spec[2]) &&
-			(format[i + 1] == spec[1]))
-		return (1);
-	else if ((format[i] == spec[2]) &&
-			(format[i + 1] == spec[0]))
-		return (2);
-	else if ((format[i] == spec[2]) && ((format[i + 1]
-					== spec[3]) || (format[i + 1] ==
-						spec[4])))
-		return (3);
-	else if ((format[i] == spec[2]) && (format[i + 1]
-					== spec[2]))
-		return (4);
-	return (5);
-}
-/**
- * _print_strin - print string
- * @c: string pointer
- * @s: pointer
- * @i: pointer
- * Return: int
- */
-int _print_strin(char *c, int *s, int *i)
-{
-	int j;
-
-	for (j = 0; c[j] != '\0'; j++)
-		;
-	write(1, c, j);
-	*i = *i + 1;
-	return (*s + j);
-}
-/**
- * _putchar - putchar
- * @c: char
- * Return: int
- */
-int _putchar(char c)
-{
-	write(1, &c, 1);
-	return (1);
 }
